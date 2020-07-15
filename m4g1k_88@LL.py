12da8ca2c349e62116de5a900054d6f0576ede4a
gauri_focaloid <gauri@focaloid.com>
2020-07-16 00:21:44 +0530

    8 ball when you don't feel so lucky!

---
+++ m4g1k_88@LL.py
@@ -0,0 +1,61 @@
+from random import randrange
+rand = randrange(20)
+if rand==0:
+    print('>>> It is certain. You are gonna get it,babe!')
+    pass
+elif rand==1:
+    print('>>> It is decidedly so. Yaaaaaaaaaass!!')
+    pass
+elif rand==2:
+    print('>>> Without a doubt. Hurraaaaaaahh!!')
+    pass
+elif rand==3:
+    print('>>> Yes. definitely. meh.')
+    pass
+elif rand==4:
+    print('>>> You may rely on it. If you wanna have "hope" ')
+    pass
+elif rand==5:
+    print('>>> As I see it, yes. Now, it depends on your shit')
+    pass
+elif rand==6:
+    print('>>> Most likely. or not. Or maybe.')
+    pass
+elif rand==7:
+    print('>>> Outlook good. Kinda. Go.')
+    pass
+elif rand==8:
+    print('>>> Yes. Meh. Probably, yes.')
+    pass
+elif rand==9:
+    print('>>> Signs point to yes. But sign also points to out.')
+    pass
+elif rand==10:
+    print('>>> Reply hazy, try again.')
+    pass
+elif rand==11:
+    print('>>> I\'m lazy. Ask again later.')
+    pass
+elif rand==12:
+    print('>>> Better not tell you now. You\'re an a\'hole' )
+    pass
+elif rand==13:
+    print('>>> Cannot predict now. I\'m bored.')
+    pass
+elif rand==14:
+    print('>>> Concentrate and ask again.')
+    pass
+elif rand==15:
+    print('>>> Don\'t count on it. Your dumb luck is out.')
+    pass
+elif rand==16:
+    print('>>> My reply is no. And, I mean no!')
+    pass
+elif rand==17:
+    print('>>> My sources say no. So, no.')
+    pass
+elif rand==18:
+    print('>>> Outlook not so good. Sooooo not good.')
+    pass
+else:
+   print('>>> Very doubtful. Go sleep.') 
